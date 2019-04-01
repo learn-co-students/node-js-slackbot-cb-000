8a695c589fa37fdcc272cf6c7d565969ebd838e0
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-01 23:43:02 +0000

    Done.

--- slack_bot.js
+++ slack_bot.js
@@ -4,14 +4,42 @@ const rp = require('request-promise');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
-const TOKEN = '[Your Slash Command Token Here]';
-
-// Just an example request to get you started..
+const fetch = require('node-fetch');
+const TOKEN = 'ZoUY133ateZvvLKmOhF9lNAd';
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.status(200).send("Hello World") ;
+})
+app.post('/', (req, res) => {
+  var explain;
+  if(req.body.token !== TOKEN){
+	res.status(400).send() ;
+  }
+  if(!req.body.text){
+	res.status(404).send("specify a user") ;
+  }
+  var ram = req.body.text.split(" ")[1] ;
+  var url = `https://api.github.com/users/${req.body.text.split(" ")[0]}` ;
+  console.log(url) ;
+  fetch(url)
+  .then(res => res.json())
+  .then(function(json){
+    if (json.message == "Not Found"){
+      explain = "user not found" ;
+      res.status(404).send(explain) ;
+    }
+    else{
+      if (!!ram){
+        var valor = json[`${ram}`] ;
+        explain = `${ram}: ${valor}` ;
+      }
+      else{
+        explain = `login: ${json.login}, name: ${json.name}, url: ${json.html_url}`
+      }
+    }
+    res.end(explain) ;
+  })
 });
-
 // This code "exports" a function 'listen` that can be used to start
 // our server on the specified port.
 exports.listen = function(port, callback) {
